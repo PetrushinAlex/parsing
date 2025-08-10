a31b2ea387592bd99af3e1ac8556f02a9acc78fa
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-08-10 11:48:47 +0300

    sprint 19

---
+++ training_parsing_yap/requests_vs_requests_cache.py
@@ -0,0 +1,21 @@
+import requests
+import requests_cache
+
+from tqdm import tqdm
+
+DELAYED_URL = 'http://httpbin.org/delay/3'
+
+if __name__ == '__main__':
+    for i in tqdm(range(3), desc='Загрузка с сервера'):
+        requests.get(DELAYED_URL)
+
+    # Сессия для работы с кешированной загрузкой.
+    session = requests_cache.CachedSession()
+    # Очистка кеша, чтобы каждый новый запуск программы
+    # не зависел от предыдущего.
+    session.cache.clear()
+
+    # Допишите к циклу прогресс-бар, дайте ему название "Загрузка из кеша".
+    for i in tqdm(range(3), desc='Загрузка из кеша'):
+        # Загрузка веб-страницы через кеширующую сессию.
+        session.get(DELAYED_URL)
